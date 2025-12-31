3e98597a5d0d7ae9705df833bf1034f1acafad92
Jaganath Sahu <jagannath8715@gmail.com>
2025-12-31 21:11:53 +0530

    implement log

--- src/main/java/com/dxc/remoteSeaProbe/lifecycle/DemoService2.java
+++ src/main/java/com/dxc/remoteSeaProbe/lifecycle/DemoService2.java
@@ -1,18 +1,21 @@
 package com.dxc.remoteSeaProbe.lifecycle;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DemoService2 {
+    private static final Logger log = LoggerFactory.getLogger(DemoService2.class);
 
     public DemoService2() {
-        System.out.println("DemoService2 constructor called");
+        log.info("DemoService2 constructor called");
     }
 
     @PostConstruct
     public void customInit() {
-        System.out.println("DemoService2 @PostConstruct customInit() called");
+        log.info("DemoService2 @PostConstruct customInit() called");
     }
 }
 
--- src/main/java/com/dxc/remoteSeaProbe/lifecycle/MyCustomBeanPostProcessor.java
+++ src/main/java/com/dxc/remoteSeaProbe/lifecycle/MyCustomBeanPostProcessor.java
@@ -1,15 +1,18 @@
 package com.dxc.remoteSeaProbe.lifecycle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyCustomBeanPostProcessor implements BeanPostProcessor {
+    private static final Logger log = LoggerFactory.getLogger(MyCustomBeanPostProcessor.class);
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) {
         if (bean instanceof DemoService) {
-            System.out.println("BeanPostProcessor BEFORE init → " + beanName);
+            log.info("BeanPostProcessor BEFORE init → " + beanName);
         }
         return bean;
     }
@@ -17,7 +20,7 @@ public class MyCustomBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) {
         if (bean instanceof DemoService) {
-            System.out.println("BeanPostProcessor AFTER init → " + beanName);
+            log.info("BeanPostProcessor AFTER init → " + beanName);
         }
         return bean;
     }
