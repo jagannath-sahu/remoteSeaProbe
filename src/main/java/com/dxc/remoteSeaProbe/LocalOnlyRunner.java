89aa8a5a734e4c2d3d2cc413c49e1c108ac370e8
Jaganath Sahu <jagannath8715@gmail.com>
2025-12-31 21:11:53 +0530

    implement log

--- src/main/java/com/dxc/remoteSeaProbe/LocalOnlyRunner.java
+++ src/main/java/com/dxc/remoteSeaProbe/LocalOnlyRunner.java
@@ -4,10 +4,13 @@ import com.dxc.remoteSeaProbe.test.SingletonService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 @Profile("local")
 public class LocalOnlyRunner implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(LocalOnlyRunner.class);
     private final SingletonService singletonService;
 
     public LocalOnlyRunner(SingletonService singletonService) {
@@ -16,7 +19,7 @@ public class LocalOnlyRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        System.out.println("LocalOnlyRunner singletonService: " + singletonService.printHash());
+        log.info("LocalOnlyRunner singletonService: {}",  singletonService.printHash());
     }
 }
 
