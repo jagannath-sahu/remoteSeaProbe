b8b459b5accd928cd9502e85fe4d4eafc2576b08
Jaganath Sahu <jagannath8715@gmail.com>
2026-01-01 08:21:24 +0530

    adding initialization logic means event

--- src/main/java/com/dxc/remoteSeaProbe/AppStartupRunner.java
+++ src/main/java/com/dxc/remoteSeaProbe/AppStartupRunner.java
@@ -1,13 +1,15 @@
 package com.dxc.remoteSeaProbe;
 
 import com.dxc.remoteSeaProbe.lifecycle.ContextAwareBean;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Component
-public class AppStartupRunner implements CommandLineRunner {
+public class AppStartupRunner implements ApplicationRunner {
     private static final Logger log = LoggerFactory.getLogger(AppStartupRunner.class);
     private final ContextAwareBean contextAwareBean;
 
@@ -16,7 +18,7 @@ public class AppStartupRunner implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
+    public void run(ApplicationArguments args) {
         log.info("AppStartupRunner started ...");
         contextAwareBean.showAllBeans();
         log.info("AppStartupRunner end ...");
